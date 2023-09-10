561ecbaa03c535112a721de261c15a3005483fdd
Trường Huỳnh <25319326+huynhphamduytruong@users.noreply.github.com>
2023-09-10 12:58:50 +0700

    feat: :lipstick: Update styles for error page
    Add new styles to handle error page when load inside layout component

--- src/pages/Shared/Layout/Layout.styled.tsx
+++ src/pages/Shared/Layout/Layout.styled.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
+import { GenericErrorContainer } from '../Error/GenericError.styled'
 
 export const StyledLayout = styled.div.attrs(({ className }) => ({
   className: `vh-100 ${className ?? ''}`.trim()
 }))`
   main > .container {
     padding: 75px 0;
+
+    &:has(${GenericErrorContainer}) {
+      height: 100vh;
+    }
   }
 `
